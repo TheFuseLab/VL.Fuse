8017abe138a5eec3d66f79a9b454651193fbd753
christian riekoff <christian@riekoff.com>
2020-11-09 17:41:13 +0100

    update project

---
+++ src/VL.ShaderFXtension/FuncFloat4InFloatOut.cs
@@ -0,0 +1,20 @@
+﻿using Stride.Core.Mathematics;
+using System.Collections.Generic;
+using Stride.Rendering.Materials;
+using Stride.Shaders;
+using VL.Stride.Shaders.ShaderFX.Functions;
+
+namespace VL.ShaderFXtension
+{
+    /// <summary>
+    /// Represents any shader that implements SDF3D with input compositions
+    /// </summary>
+    public class FuncFloat4InFloatOut : Funk1In1Out<Vector4, float>
+    {
+        public FuncFloat4InFloatOut(string functionName, IEnumerable<KeyValuePair<string, IComputeNode>> inputs)
+            : base(functionName, inputs)
+        {
+
+        }
+    }
+}
---
+++ src/VL.ShaderFXtension/UnaryOperationT1InT2Out.cs
@@ -0,0 +1,44 @@
+﻿
+
+using System.Collections.Generic;
+using Stride.Rendering.Materials;
+using Stride.Shaders;
+using VL.Stride.Shaders.ShaderFX;
+using static VL.Stride.Shaders.ShaderFX.ShaderFXUtils;
+
+namespace VL.ShaderFXtension
+{
+   
+        public class UnaryOperationT1InT2Out<TIn, TOut> : ComputeValue<TOut>
+        {
+            public UnaryOperationT1InT2Out(string operatorName, IComputeValue<TIn> value)
+            {
+                ShaderName = operatorName;
+                Value = value;
+            }
+
+            public IComputeValue<TIn> Value { get; }
+
+            public override IEnumerable<IComputeNode> GetChildren(object context = null)
+            {
+                return ReturnIfNotNull(Value);
+            }
+
+            public override ShaderSource GenerateShaderSource(ShaderGeneratorContext context, MaterialComputeColorKeys baseKeys)
+            {
+                var shaderSource = GetShaderSourceForType2<TIn, TOut>(ShaderName);
+
+                var mixin = shaderSource.CreateMixin();
+
+                mixin.AddComposition(Value, "Value", context, baseKeys);
+
+                return mixin;
+            }
+
+            public override string ToString()
+            {
+                return string.Format("Op {0} {1}", Value, ShaderName);
+            }
+        }
+    
+}
