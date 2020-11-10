f7caa921965d9d131b563791ea223570f0d6633b
christian riekoff <christian@riekoff.com>
2020-11-10 16:41:28 +0100

    add experiment to generate shader from source

---
+++ src/VL.ShaderFXtension/FloatComputeNode.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Stride.Rendering.Materials;
+using Stride.Shaders;
+using VL.Stride.Shaders.ShaderFX;
+
+namespace VL.ShaderFXtension
+{
+    public class FloatComputeNode : GenericComputeNode<float>
+    {
+        public FloatComputeNode(Func<ShaderGeneratorContext, ShaderClassCode> getShaderSource, IEnumerable<KeyValuePair<string, IComputeNode>> inputs) : base(getShaderSource, inputs)
+        {
+        }
+    }
+}
---
+++ src/VL.ShaderFXtension/TypedComputeNode.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Stride.Rendering.Materials;
+using Stride.Shaders;
+using VL.Stride.Shaders.ShaderFX;
+
+namespace VL.ShaderFXtension
+{
+    public class TypedComputeNode<T> :ComputeValue<T>
+    {
+        public TypedComputeNode(Func<ShaderGeneratorContext, MaterialComputeColorKeys, ShaderClassCode> getShaderSource,
+            IEnumerable<KeyValuePair<string, Var<T>>> inputs)
+        {
+            Inputs = inputs?.Where(input => !string.IsNullOrWhiteSpace(input.Key) && input.Value != null).ToList();
+            GetShaderSource = getShaderSource;
+        }
+
+        public Func<ShaderGeneratorContext, MaterialComputeColorKeys, ShaderClassCode> GetShaderSource { get; }
+
+        public IEnumerable<KeyValuePair<string, Var<T>>> Inputs { get; }
+
+        //public ParameterCollection Parameters => parameters;
+        public ShaderClassCode ShaderClass { get; private set; }
+
+        //ParameterCollection parameters;
+
+        public override ShaderSource GenerateShaderSource(ShaderGeneratorContext context, MaterialComputeColorKeys baseKeys)
+        {
+            //parameters = context.Parameters;
+            ShaderClass = GetShaderSource(context, baseKeys);
+
+            //compose if necessary
+            if (Inputs == null || !Inputs.Any()) return ShaderClass;
+            
+            var mixin = ShaderClass.CreateMixin();
+
+            foreach (var input in Inputs)
+            {
+                mixin.AddComposition(input.Value, input.Key, context, baseKeys);
+            }
+
+            return mixin;
+
+        }
+
+        public override IEnumerable<IComputeNode> GetChildren(object context = null)
+        {
+            if (Inputs == null) yield break;
+            
+            foreach (var item in Inputs)
+            {
+                if (item.Value != null)
+                    yield return item.Value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return ShaderClass?.ToString() ?? GetType().ToString();
+        }
+    }
+}
