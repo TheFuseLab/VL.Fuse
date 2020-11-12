51395949a4cbc1f4f8d0a8cfd29b5bdbbaa34130
Christian Riekoff <christian@riekoff.com>
2020-11-12 11:51:30 +0100

    add addshader source

--- src/VL.ShaderFXtension/EffectSystemExtensions.cs
+++ src/VL.ShaderFXtension/EffectSystemExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Reflection;
 using Stride.Core.IO;
 using Stride.Rendering;
+using Stride.Shaders.Compiler;
+using Stride.Shaders.Parser;
 using static Stride.Shaders.Compiler.EffectCompilerFactory;
 
 namespace VL.ShaderFXtension
@@ -13,6 +16,24 @@ namespace VL.ShaderFXtension
             var inMemoryFileProvider = new InMemoryFileProvider(databaseProvider.FileProvider);
             effectSystem.Services.AddService(inMemoryFileProvider);
             effectSystem.Compiler = CreateEffectCompiler(inMemoryFileProvider, effectSystem, database: databaseProvider.FileProvider);
+        }
+
+        private static HashSet<string> addedShaders = new HashSet<string>();
+        
+        public static void AddShaderSource(this EffectSystem effectSystem, string type, string sourceCode, string sourcePath)
+        {
+            if (addedShaders.Contains(type)) return;
+
+            var compiler = effectSystem.Compiler as EffectCompiler;
+            if (compiler is null && effectSystem.Compiler is EffectCompilerCache effectCompilerCache)
+                compiler = typeof(EffectCompilerChain).GetProperty("Compiler", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(effectCompilerCache) as EffectCompiler;
+            
+            if (compiler == null) return;
+            
+            var getParserMethod = typeof(EffectCompiler).GetMethod("GetMixinParser", BindingFlags.Instance | BindingFlags.NonPublic);
+            var parser = getParserMethod.Invoke(compiler, null) as ShaderMixinParser;
+            var sourceManager = parser.SourceManager;
+            sourceManager.AddShaderSource(type, sourceCode, sourcePath);
         }
 
         public static void RegisterShader(this EffectSystem effectSystem, string path, string shader)
