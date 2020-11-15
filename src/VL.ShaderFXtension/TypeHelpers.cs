245836964577895c6239802415714a581a0012aa
Christian Riekoff <christian@riekoff.com>
2020-11-15 12:42:28 +0100

    add helper class to constraint types

---
+++ src/VL.ShaderFXtension/TypeHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace VL.ShaderFXtension
+{
+    
+        public static class TypeHelpers
+        {
+            
+
+            public static void ConstrainTypes<T>(T input, T input2, T input3, T input4)
+            {
+            }
+            
+            public static void ConstrainTypes<T>(T input, IEnumerable<T> input2)
+            {
+            }
+        }
+}
