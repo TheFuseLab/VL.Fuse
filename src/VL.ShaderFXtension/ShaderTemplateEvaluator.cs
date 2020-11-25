cb932c096c6c255b9aaf255d791addc9b9624a83
christian riekoff <christian@riekoff.com>
2020-11-26 00:03:22 +0100

    add function to evaluate shaders and replace string formating with some smarter keybased regex matching template evaluation

---
+++ src/VL.ShaderFXtension/ShaderTemplateEvaluator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using Stride.Animations;
+
+namespace VL.ShaderFXtension
+{
+    public class ShaderTemplateEvaluator
+    {
+        public static string Evaluate(string theShaderTemplate, Dictionary<string,string> theKeys)
+        {
+            return Regex.Replace(
+                theShaderTemplate, 
+                @"\$\{(?<key>[^}]+)\}", 
+                m => theKeys.ContainsKey(m.Groups["key"].Value) ? theKeys[m.Groups["key"].Value] : m.Value
+                );
+        }
+    }
+}
