5a3db50654f26a7511fa0e6028db89adce44afe6
christian riekoff <christian@riekoff.com>
2021-01-30 02:20:25 +0100

    add adaptive from float node

--- src/Fuse/FromFloat.cs
+++ src/Fuse/FromFloat.cs
@@ -1,7 +1,39 @@
-﻿namespace Fuse
+﻿using System.Collections.Generic;
+
+namespace Fuse
 {
-    public class FromFloat
+    
+    public class FromFloat<T> : ShaderNode<T>
     {
-        
+
+        public FromFloat(GpuValue<float> x) : base("fromFloat")
+        {
+            x = x ?? new ConstantValue<float>(0);
+
+            var code = "";
+            switch (TypeHelpers.GetDimension(typeof(T)))
+            {
+                case 1:
+                    code = "float ${resultName} = ${x};";
+                    break;
+                case 2:
+                    code = "float2 ${resultName} = float2(${x},${x});";
+                    break;
+                case 3:
+                    code = "float3 ${resultName} = float3(${x},${x},${x});";
+                    break;
+                case 4:
+                    code = "float4 ${resultName} = float4(${x},${x},${x},${x});";
+                    break;
+            }
+            
+            Setup(
+                code, 
+                new List<AbstractGpuValue>{x},
+                new Dictionary<string, string>
+                {
+                    {"x", x.ID},
+                });
+        }
     }
 }
