0ac3943b0fb454d9b591eae2eb43c4286a7f4c8d
christian riekoff <christian@riekoff.com>
2021-02-03 14:35:55 +0100

    improved constant value

--- src/Fuse/ConstantValue.cs
+++ src/Fuse/ConstantValue.cs
@@ -4,22 +4,27 @@ using Stride.Core.Mathematics;
 namespace Fuse
 {
 
+    public static class ConstantHelper
+    {
+        // ReSharper disable once UnusedMember.Global
+        // USED IN VL
+        public static ConstantValue<T> FromFloat<T>(float theValue)
+        {
+            if (typeof(T) == typeof(float)) return new ConstantValue<T>((T)Convert.ChangeType(theValue, typeof(float)));
+            if (typeof(T) == typeof(Vector2)) return new ConstantValue<T>((T)Convert.ChangeType(new Vector2(theValue,theValue), typeof(Vector2)));
+            if (typeof(T) == typeof(Vector3)) return new ConstantValue<T>((T)Convert.ChangeType(new Vector3(theValue,theValue,theValue), typeof(Vector3)));
+            if (typeof(T) == typeof(Vector4)) return new ConstantValue<T>((T)Convert.ChangeType(new Vector4(theValue,theValue,theValue,theValue), typeof(Vector4)));
+            return null;
+        }
+    }
     
     public class ConstantValue<T>: GpuValue<T>
     {
+        
         private T _myValue;
         public ConstantValue(T theValue) : base("constant")
         {
             _myValue = theValue;
-        }
-        
-        public ConstantValue(float theValue): base("constant")
-        {
-           
-                if (typeof(T) == typeof(float)) _myValue = (T)Convert.ChangeType(theValue, typeof(float));
-                if (typeof(T) == typeof(Vector2))_myValue = (T)Convert.ChangeType(new Vector2(theValue,theValue), typeof(Vector2));
-                if (typeof(T) == typeof(Vector3))_myValue = (T)Convert.ChangeType(new Vector3(theValue,theValue,theValue), typeof(Vector3));
-                if (typeof(T) == typeof(Vector4))_myValue = (T)Convert.ChangeType(new Vector4(theValue,theValue,theValue,theValue), typeof(Vector4));
         }
         
         public override string ID => TypeHelpers.GetDefaultForType<T>(_myValue);
