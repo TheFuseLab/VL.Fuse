98b23b3aa5273370a8226289e19bc32ba1c14ce1
christian riekoff <christian@riekoff.com>
2021-02-09 20:02:52 +0100

    Add Particle Attributes Dynamic Enum to play around

---
+++ src/Fuse/particles/ParticleAttribute.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using VL.Lib.Collections;
+
+namespace Fuse.particles
+{
+   
+    
+    public class ParticleAttributeManager
+    {
+        public string Update(ParticleAttribute enumInput)
+        {
+            if (enumInput.IsValid())
+                return enumInput.Definition.Entries[enumInput.SelectedIndex()];
+            else
+                return "No valid entry selected";
+        }
+
+        public ParticleAttributeDefinition GetDefinition()
+        {
+            return ParticleAttributeDefinition.Instance;
+        }
+
+        public void AddEnumEntry(string entry)
+        {
+            ParticleAttributeDefinition.Instance.AddEntry(entry, null);
+        }
+
+        public void RemoveEnumEntry(string entry)
+        {
+            ParticleAttributeDefinition.Instance.RemoveEntry(entry);
+        }
+    }
+
+    public class ParticleAttributeDefinition : ManualDynamicEnumDefinitionBase<ParticleAttributeDefinition>
+    {
+        //this is optional an can be used if any initialization before the call to GetEntries is needed
+        protected override void Initialize()
+        {
+        }
+
+        //add this to get a node that can access the Instance from everywhere
+        public static ParticleAttributeDefinition Instance => ManualDynamicEnumDefinitionBase<ParticleAttributeDefinition>.Instance;
+    }
+
+    [Serializable]
+    public class ParticleAttribute: DynamicEnumBase<ParticleAttribute, ParticleAttributeDefinition>
+    {
+        public ParticleAttribute(string value) : base(value)
+        {
+        }
+
+        //this method needs to be imported in VL to set the default
+        public static ParticleAttribute CreateDefault()
+        {
+            //use method of base class if nothing special required
+            return CreateDefaultBase();
+        }
+    }
+}
