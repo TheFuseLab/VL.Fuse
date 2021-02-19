7f2f0600421fe00ce7e2cc91e94b42d67fc33430
everyoneishappy <everyoneishappy@yahoo.com>
2021-02-19 11:16:21 +1000

    Add General Distance Metric Enum

---
+++ src/Fuse/Enums/DistanceMetrics.cs
@@ -0,0 +1,14 @@
+﻿namespace Fuse.math
+{
+    public enum DistanceMetrics
+    {
+        Euclidean,
+        EuclideanSquared,
+        Chebyshev,
+        Manhattan,
+        Canberra,
+        CosineSimilarity,
+        PearsonCorrelation,
+        Minkowski //not sure about this guy, since changes the complicates the signature 
+    }
+}
