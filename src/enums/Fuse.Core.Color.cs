d49feefd783af51cc2aded371f4f8441c3267bbc
christian riekoff <christian@riekoff.com>
2021-04-09 19:57:56 +0200

    add more enums

--- src/enums/Fuse.Core.Color.cs
+++ src/enums/Fuse.Core.Color.cs
@@ -2,5 +2,33 @@
 
 namespace Fuse.Core.Color
 {
-    public enum ColorSpace { RGB, HSL, HSV, LAB };
+    public enum ColorSpace { RGB, HSL, HSV, LAB, SRGB };
+
+    public enum BlendMode
+    {
+        Normal,
+        
+        Darken,
+        Multiply,
+        ColorBurn,
+        LinearBurn,
+        
+        Lighten,
+        Screen,
+        Dodge,
+        LinearDodge,
+        
+        Overlay,
+        SoftLight,
+        HardLight,
+        VividLight,
+        LinearLight,
+        PinLight,
+        HardMix,
+        
+        Difference,
+        Exclusion,
+        Subtract,
+        Divide
+    }
 }
