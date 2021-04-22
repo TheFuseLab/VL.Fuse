44fb6e3b04e49667130179579fe41cd1a1ba2054
christian riekoff <christian@riekoff.com>
2021-04-22 18:44:40 +0200

    let set attribute return the struct

--- src/Fuse/compute/DynamicStructSetAttribute.cs
+++ src/Fuse/compute/DynamicStructSetAttribute.cs
@@ -2,26 +2,27 @@
 
 namespace Fuse.compute
 {
-    public class DynamicStructSetAttribute<T> : ShaderNode<GpuVoid>
+    public class DynamicStructSetAttribute<T> : ShaderNode<GpuStruct>
     {
 
         private readonly GpuValue<GpuStruct> _struct;
-        private readonly GpuValue<T> _member;
+        private readonly string _member;
         private readonly GpuValue<T> _value;
         
-        public DynamicStructSetAttribute(GpuValue<GpuStruct> theStruct, GpuValue<T> theMember, GpuValue<T> theValue) : base("StructAttribute")
+        public DynamicStructSetAttribute(GpuValue<GpuStruct> theStruct, string theMember, GpuValue<T> theValue) : base("StructAttribute")
         {
             _struct = theStruct;
             _member = theMember;
             _value = theValue;
             Setup(new List<AbstractGpuValue>{theStruct,theValue});
+            Output = theStruct;
         }
 
         protected override string SourceTemplate()
         {
             return ShaderNodesUtil.Evaluate("${input}.${member} =  ${value};",new Dictionary<string, string> {
-                {"input", _struct==null? "" : _struct.ID},
-                {"member", _member.Name},
+                {"input", _struct == null? "" : _struct.ID},
+                {"member", _member},
                 {"value", _value.ID}
             });
         }
