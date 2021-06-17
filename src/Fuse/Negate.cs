37500caf255dcb48ecc311eb26e0945c84fac5be
christian riekoff <christian@riekoff.com>
2021-06-17 15:43:51 +0200

    added negate node

--- src/Fuse/Negate.cs
+++ src/Fuse/Negate.cs
@@ -1,7 +1,25 @@
-﻿namespace Fuse
+﻿using System.Collections.Generic;
+
+namespace Fuse
 {
-    public class Negate
+    public class Negate<T> : ShaderNode<T>
     {
+        private GpuValue<T> _in;
+
+        public Negate(GpuValue<T> theIn) : base("negate")
+        {
+            _in = theIn ??  ConstantHelper.FromFloat<T>(0);
+            
+            Setup( new List<AbstractGpuValue>{_in});
+        }
         
+        protected override string SourceTemplate()
+        {
+            return ShaderNodesUtil.Evaluate("${resultType} ${resultName} = -${in};", 
+                new Dictionary<string, string>
+                {
+                    {"in", _in.ID},
+                });
+        }
     }
 }
