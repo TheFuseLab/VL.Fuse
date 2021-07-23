9d37e8abae0dfc408dd3896983bc157ecdc54c54
everyoneishappy <everyoneishappy@yahoo.com>
2021-07-23 10:54:13 +1000

    Add Calc Enums

---
+++ src/Fuse/enums/Fuse.Core.Calculus.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Fuse.Core.Calculus
+{
+    public enum GradientDifferentiationMode { ForwardDifference, CentralDifference, FivePointStencil };
+
+    public enum IntegrationnMode { Euler, RungeKutta2, RungeKutta4 };
+
+}
