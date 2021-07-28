4532b5998a773d1ac65eb8c9f75a027dff2c6880
christian riekoff <christian@riekoff.com>
2021-07-28 12:12:40 +0200

    add Not Node

---
+++ src/Fuse/Not.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Fuse
+{
+    public class Not: ShaderNode<bool>
+    {
+        private readonly GpuValue<bool> _in;
+
+        public Not(GpuValue<bool> theIn) : base("not")
+        {
+            _in = theIn ??  new ConstantValue<bool>(false);
+            
+            Setup( new List<AbstractGpuValue>{_in});
+        }
+        
+        protected override string SourceTemplate()
+        {
+            return ShaderNodesUtil.Evaluate("${resultType} ${resultName} = !${in};", 
+                new Dictionary<string, string>
+                {
+                    {"in", _in.ID},
+                });
+        }
+    }
+}
