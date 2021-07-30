1f571d09173dccb98853ec07463310bce00ada81
Christian Riekoff <christian@riekoff.com>
2021-07-30 10:36:33 +0200

    add more generic texture handling

--- src/Fuse/SampleTextureNode.cs
+++ src/Fuse/SampleTextureNode.cs
@@ -4,14 +4,14 @@ using Stride.Graphics;
 
 namespace Fuse
 {
-    public class SampleTextureNode : ShaderNode<Vector4>
+    public class SampleTextureNode<TTexCoord> : ShaderNode<Vector4>
     {
 
         private readonly GpuValue<Texture> _texture;
         private readonly GpuValue<SamplerState> _sampler;
-        private readonly GpuValue<Vector2> _texCoord;
+        private readonly GpuValue<TTexCoord> _texCoord;
         private readonly GpuValue<float> _level;
-        public SampleTextureNode(GpuValue<Texture> theTexture, GpuValue<SamplerState> theSampler, GpuValue<Vector2> theTexCoords, GpuValue<float> theLevel, ConstantValue<Vector4> theDefault) : base( "sampleTexture", theDefault)
+        public SampleTextureNode(GpuValue<Texture> theTexture, GpuValue<SamplerState> theSampler, GpuValue<TTexCoord> theTexCoords, GpuValue<float> theLevel, ConstantValue<Vector4> theDefault) : base( "sampleTexture", theDefault)
         {
             _texture = theTexture;
             _sampler = theSampler;
