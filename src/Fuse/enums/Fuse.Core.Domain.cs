f4e0c15909736553bb9a30a31cab68bc98f35406
everyoneishappy <everyoneishappy@yahoo.com>
2021-08-10 17:12:33 +1000

    Work On Domain Nodes

--- src/Fuse/enums/Fuse.Core.Domain.cs
+++ src/Fuse/enums/Fuse.Core.Domain.cs
@@ -4,7 +4,7 @@ namespace Fuse.Core.Domain
 {
     public enum DistanceMetric { Euclidean, EuclideanSquared, Chebyshev, Manhattan, Canberra, CosineSimilarity, PearsonCorrelation, Minkowski };
 
-    public enum DomainRepat2D { WrapXY, MirrorXY, GridXY, WrapX, MirrorX, WrapY, MirrorY, PolarXY };
+    public enum DomainRepeat2D { WrapXY, MirrorXY, GridXY, WrapX, MirrorX, WrapY, MirrorY, PolarXY };
 
-    public enum DomainRepat3D { WrapX, WrapY, WrapZ, MirrorX, MirrorY, MirrorZ, WrapXY, WrapXZ, WrapYZ, MirrorXY, MirrorXZ, MirrorYZ, WrapXYZ, PolarXY, PolarXZ, PolarYZ };
+    public enum DomainRepeat3D { WrapX, WrapY, WrapZ, MirrorX, MirrorY, MirrorZ, WrapXY, WrapXZ, WrapYZ, MirrorXY, MirrorXZ, MirrorYZ, WrapXYZ, PolarXY, PolarXZ, PolarYZ };
 }
