114b51c75f90ca9d5ce4832fad92e4c42475d109
christian riekoff <christian@riekoff.com>
2021-08-17 16:30:43 +0200

    add experimental coded compute system version

---
+++ src/Fuse/ComputeSystem/IResource.cs
@@ -0,0 +1,11 @@
+﻿namespace Fuse.ComputeSystem
+{
+    public interface IResource
+    {
+        public void CreateResources();
+
+        public void AddListener(IResourceListener theListener);
+
+        public void AddAttribute(IMember theMember);
+    }
+}
---
+++ src/Fuse/ComputeSystem/ResourceManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Stride.Core.Yaml.Tokens;
+
+namespace Fuse.ComputeSystem
+{
+    public class ResourceManager
+    {
+        public delegate IResourceBinder CreateBinder(IResource theResource);
+
+        private readonly Dictionary<string, IResourceBinder> _resources = new Dictionary<string, IResourceBinder>();
+        
+        public ResourceManager(IComputeStage _computeStage, Dictionary<string, IResource> theResources, CreateBinder theBinder)
+        {
+            theResources.ForEach(kv => _resources[kv.Key] = theBinder.Invoke(kv.Value));
+            
+            _computeStage.Resources().ForEach(kv =>
+            {
+                if (!_resources.ContainsKey(kv.Key)) return;
+
+                var binder = _resources[kv.Key];
+                
+                kv.Value.ForEach(attribute =>
+                {
+                    attribute.Reference.SetAbstractInput(binder.GetAttribute(attribute.Member.Name));
+                });
+            });
+        }
+    }
+}
