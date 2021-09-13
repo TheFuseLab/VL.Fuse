bd556d8e25a39339586b7abc06dda28908c6e3a4
tebjan <tebjan@vvvv.org>
2021-09-14 00:28:59 +0200

    Adds PatchTests project

---
+++ PatchTests/UnitTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace MyTests
+{
+    public class UnitTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(1 + 1, 2, "An old contract people tell their kids about just failed. Math is useless from now on.");
+        }
+    }
+}
