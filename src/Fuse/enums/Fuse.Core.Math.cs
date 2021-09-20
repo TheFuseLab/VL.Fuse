48a4040591e1dcdcac4638acc96906eccdddaf3a
everyoneishappy <everyoneishappy@yahoo.com>
2021-09-20 10:21:45 +1000

    Add easings Enums

--- src/Fuse/enums/Fuse.Core.Math.cs
+++ src/Fuse/enums/Fuse.Core.Math.cs
@@ -1,4 +1,38 @@
 ﻿namespace Fuse.Core.Math
 {
     public enum Constrain { Off, Clamp, Wrap, Mirror };
+    public enum EaseMode 
+    {
+        backInOut,
+        backIn,
+        backOut,
+        bounceOut,
+        bounceIn,
+        bounceInOut,
+        circularInOut,
+        circularIn,
+        circularOut,
+        cubicInOut,
+        cubicIn,
+        cubicOut,
+        elasticInOut,
+        elasticIn,
+        elasticOut,
+        exponentialInOut,
+        exponentialIn,
+        exponentialOut,
+        linear,
+        quadraticInOut,
+        quadraticIn,
+        quadraticOut,
+        quarticInOut,
+        quarticIn,
+        quarticOut,
+        qinticInOut,
+        qinticIn,
+        qinticOut,
+        sineInOut,
+        sineIn,
+        sineOut
+    };
 }
