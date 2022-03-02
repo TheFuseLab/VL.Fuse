fe0dc32e37b6c816f8d7347ef4e7686d52efd5d1
tebjan <tebjan@vvvv.org>
2022-03-02 18:24:30 +0100

    Updates to gamma 2021.4.7 so tests run again

--- PatchTests/PatchTests.cs
+++ PatchTests/PatchTests.cs
@@ -22,7 +22,7 @@ namespace MyTests
     public class PatchTests
     {
 
-        static string VLVersion = "2021.4.0-0568-gba571a190a";
+        static string VLVersion = "2021.4.7";
         static string[] Packs = new string[]{ 
         
         //  FIX ME !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
