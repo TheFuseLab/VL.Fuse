e3579d3f1b68afb1b33d101ae9905df68580aa05
Christian Riekoff <christian@riekoff.com>
2022-04-20 15:00:25 +0200

    add expand node

---
+++ src/Fuse/FloatExpand.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Fuse
+{
+    public class FloatExpand<TIn,TOut> : ShaderNode<TOut>
+    {
+        private readonly GpuValue<TIn> _in;
+        private readonly GpuValue<float> _expand;
+
+        public FloatExpand(GpuValue<TIn> x, GpuValue<float> y) : base("floatExpand")
+        {
+            _in = x ?? ConstantHelper.FromFloat<TIn>(0);
+            _expand = y ?? new ConstantValue<float>(0);
+            
+            Setup( new List<AbstractGpuValue>{_in,_expand});
+        }
+        
+        protected override string SourceTemplate()
+        {
+            return ShaderNodesUtil.Evaluate("${resultType} ${resultName} = ${resultType}(${in},${expand});", 
+                new Dictionary<string, string>
+                {
+                    {"in", _in.ID},
+                    {"expand", _expand.ID}
+                });
+        }
+    }
+}
