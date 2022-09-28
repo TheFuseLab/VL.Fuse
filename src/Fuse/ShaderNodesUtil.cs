8c847d05d8cc180732dd279dd23fb4c2aa9a6d17
Christian Riekoff <christian@riekoff.com>
2022-09-28 11:04:27 +0200

    add persitent hasch code generation

--- src/Fuse/ShaderNodesUtil.cs
+++ src/Fuse/ShaderNodesUtil.cs
@@ -359,18 +359,33 @@ namespace Fuse
             return Id2++;
         }
         
+        /**
+         * To be checked if hashes are persistent and individual
+         * 
+         */
+        public static int GetStableHashCode(this string str)
+        {
+            unchecked
+            {
+                var hash1 = 5381;
+                var hash2 = hash1;
+
+                for(var i = 0; i < str.Length && str[i] != '\0'; i += 2)
+                {
+                    hash1 = ((hash1 << 5) + hash1) ^ str[i];
+                    if (i == str.Length - 1 || str[i+1] == '\0')
+                        break;
+                    hash2 = ((hash2 << 5) + hash2) ^ str[i+1];
+                }
+
+                return hash1 + (hash2*1566083941);
+            }
+        }
+        
         public static uint GetHashCode(NodeContext nodeContext)
         {
-            var myHashCode = new HashCode();
-
-            if (nodeContext == null) return (uint) myHashCode.ToHashCode();
-            
-            foreach (var p in nodeContext.Path.Stack)
-            {
-                myHashCode.Add(p.ElementId);
-            }
             unchecked{
-                return (uint)myHashCode.ToHashCode();
+                return (uint)GetStableHashCode(nodeContext.Path.ToString());
             }
         }
 
