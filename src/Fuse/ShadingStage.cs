0bd5340413756a05b9781474f8e086834b019d70
Christian Riekoff <christian@riekoff.com>
2022-12-12 10:36:01 +0100

    add shading stage

---
+++ src/Fuse/ShadingStage.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Fuse.ShaderFX;
+
+namespace Fuse
+{
+    public class ShadingStage : AbstractStage
+    {
+        private const string ShaderSource = @"
+    //override shading, create sphere impostor in this case
+	stage override float4 Shading()
+	{
+        ${sourceShading}
+		return ${resultShading};
+	}";
+        
+        public ShadingStage(AbstractShaderNode theShaderNode) : base("Shading", theShaderNode)
+        {
+        }
+
+        public override void AppendInputs(Dictionary<string, string> theTemplateMap)
+        {
+            theTemplateMap["resultShading"] = StageNode.ID +";";
+        }
+
+        public override string Source()
+        {
+            return ShaderSource;
+        }
+    }
+}
