c8280a752e2c600612e3042a01f0ebaf3f62c1ce
Christian Riekoff <christian@riekoff.com>
2022-12-20 14:39:40 +0100

    update attribute code

---
+++ src/Fuse/compute/StructuredBufferAttribute.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using Fuse.ComputeSystem;
+using Stride.Graphics;
+using VL.Core;
+
+namespace Fuse.compute
+{
+    public enum StructuredBufferAttributeType
+    {
+        Attribute,
+        Resource,
+        Instance
+    }
+
+    public interface IStructureBufferAttribute : IAttribute
+    {
+        public string Group { get; }
+
+        public StructuredBufferAttributeType StructuredBufferAttributeType();
+    }
+
+    public class StructuredBufferAttribute<T> : Attribute<T>, IStructureBufferAttribute
+    {
+       // public ShaderNode<T> Default;
+        public StructuredBufferAttribute(NodeContext nodeContext, string theGroup, string theName, bool theDefineSemantic = true, ShaderNode<T> theDefault = null) : base(nodeContext, theName, AttributeType.StructuredBuffer)
+        {
+            var myFactory = new NodeSubContextFactory(nodeContext);
+            Group = theGroup;
+
+            Default = theDefault ?? ConstantHelper.FromFloat<T>(myFactory.NextSubContext(), 0f);
+            
+            SetInput(new Semantic<T>(myFactory.NextSubContext(),theName, theDefineSemantic, theName.ToUpper()));
+            SetProperty("ComputeSystemAttribute", this);
+        }
+        
+        public string Group { get; }
+        public StructuredBufferAttributeType StructuredBufferAttributeType()
+        {
+            return compute.StructuredBufferAttributeType.Attribute;
+        }
+
+        public void OverRideByInstance(ShaderNode<GpuStruct> theInstance)
+        {
+            if (theInstance == null)
+            {
+                SetProperty("ComputeSystemAttribute", this);
+            }else{
+                var myFactory = new NodeSubContextFactory(NodeContext,1);
+                var getMember = new GetMember<GpuStruct, T>(myFactory.NextSubContext());
+                getMember.SetInput(Name, theInstance);
+                SetInput(getMember);
+                RemoveProperty("ComputeSystemAttribute");
+            }
+            CallChangeEvent();
+        }
+
+        public List<string> Description {
+            get { 
+                var keys = new List<string> { "x", "y", "z", "w"};
+                var dimension = TypeHelpers.GetDimension<T>();
+                var result = new List<string>();
+                if (dimension == 1)
+                {
+                    result.Add(Name);
+                }else{
+                    for (var i = 0; i < dimension;i++)
+                    {
+                        result.Add(Name +"." + keys[i]);
+                    }
+                }
+                return result;
+            } }
+    }
+    
+    public class StructuredBufferResource : Attribute<Buffer>, IStructureBufferAttribute, IBufferInput<GpuStruct>
+    {
+
+
+
+        public StructuredBufferResource(NodeContext nodeContext, string theName) : base(nodeContext, theName, AttributeType.StructuredBuffer)
+        {
+            AddProperty("ComputeSystemAttribute", this);
+        }
+
+        public string Group => Name;
+
+        public StructuredBufferAttributeType StructuredBufferAttributeType()
+        {
+            return compute.StructuredBufferAttributeType.Resource;
+        }
+
+        public bool Append {
+            set { }
+        }
+    }
+    
+    public class StructuredBufferInstance : Attribute<GpuStruct>, IStructureBufferAttribute
+    {
+
+
+
+        public StructuredBufferInstance(NodeContext nodeContext, string theName) : base(nodeContext, theName, AttributeType.StructuredBuffer)
+        {
+            AddProperty("ComputeSystemAttribute", this);
+        }
+
+        public string Group => Name;
+
+        public StructuredBufferAttributeType StructuredBufferAttributeType()
+        {
+            return compute.StructuredBufferAttributeType.Instance;
+        }
+    }
+}
---
+++ src/Fuse/compute/TextureAttribute.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using Fuse.ComputeSystem;
+using Stride.Graphics;
+using VL.Core;
+
+namespace Fuse.compute
+{
+    public enum TextureAttributeType
+    {
+        Attribute,
+        Resource,
+        Instance
+    }
+
+    public interface ITextureAttribute : IAttribute
+    {
+        public string Group { get; }
+        
+        public bool DoubleBuffered { get; }
+
+        public TextureAttributeType TextureAttributeType();
+    }
+
+    public class TextureAttribute<TIndex,T> : Attribute<T>, ITextureAttribute where T : struct
+    {
+       // public ShaderNode<T> Default;
+        public TextureAttribute(NodeContext nodeContext, string theGroup, string theName, bool theIsDoubleBuffered, ShaderNode<T> theDefault = null) : base(nodeContext, theName, AttributeType.Texture)
+        {
+            var myFactory = new NodeSubContextFactory(nodeContext);
+            Group = theGroup;
+            DoubleBuffered = theIsDoubleBuffered;
+            Default = theDefault ?? ConstantHelper.FromFloat<T>(myFactory.NextSubContext(), 0f);
+            
+            SetProperty("ComputeSystemAttribute", this);
+        }
+
+        public bool DoubleBuffered { get; }
+        public string Group { get; }
+        public TextureAttributeType TextureAttributeType()
+        {
+            return compute.TextureAttributeType.Attribute;
+        }
+
+        public void OverRideByTexture(ShaderNode<Texture> theInstance, ShaderNode<TIndex> theIndex)
+        {
+            if (theInstance == null)
+            {
+                SetProperty("ComputeSystemAttribute", this);
+            }else{
+                var myFactory = new NodeSubContextFactory(NodeContext,1);
+                var textureGet = new ComputeTextureGet<TIndex, T>(myFactory.NextSubContext());
+                textureGet.SetInputs(theInstance, theIndex);
+                SetInput(textureGet);
+                RemoveProperty("ComputeSystemAttribute");
+            }
+            CallChangeEvent();
+        }
+    }
+    
+    public class TextureResource : Attribute<Texture>, ITextureAttribute
+    {
+
+
+
+        public TextureResource(NodeContext nodeContext, string theName) : base(nodeContext, theName, AttributeType.Texture)
+        {
+            AddProperty("ComputeSystemAttribute", this);
+        }
+
+        public string Group => Name;
+
+        public TextureAttributeType TextureAttributeType()
+        {
+            return compute.TextureAttributeType.Resource;
+        }
+
+        public bool DoubleBuffered => true;
+    }
+    
+    public class TextureInstance<T> : Attribute<T>, ITextureAttribute
+    {
+
+
+
+        public TextureInstance(NodeContext nodeContext, string theName) : base(nodeContext, theName, AttributeType.Texture)
+        {
+            AddProperty("ComputeSystemAttribute", this);
+        }
+
+        public string Group => Name;
+
+        public TextureAttributeType TextureAttributeType()
+        {
+            return compute.TextureAttributeType.Instance;
+        }
+        
+        public bool DoubleBuffered => true;
+    }
+}
