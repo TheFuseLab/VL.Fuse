80f640845eef978554f85153b05c536678206bd4
Christian Riekoff <christian@riekoff.com>
2023-01-04 14:41:07 +0100

    removed instance and resource texture attribute as they are not needed

--- src/Fuse/compute/TextureAttribute.cs
+++ src/Fuse/compute/TextureAttribute.cs
@@ -1,24 +1,18 @@
 ﻿using System.Collections.Generic;
 using Fuse.ComputeSystem;
+using Stride.Core.Mathematics;
 using Stride.Graphics;
 using VL.Core;
 
 namespace Fuse.compute
 {
-    public enum TextureAttributeType
-    {
-        Attribute,
-        Resource,
-        Instance
-    }
+    
 
     public interface ITextureAttribute : IAttribute
     {
         public string Group { get; }
         
         public bool DoubleBuffered { get; }
-
-        public TextureAttributeType TextureAttributeType();
 
         public TextureInput TextureInput{
             get;
@@ -34,7 +28,7 @@ namespace Fuse.compute
             var myFactory = new NodeSubContextFactory(nodeContext);
             Group = theGroup;
             DoubleBuffered = theIsDoubleBuffered;
-            Default = theDefault ?? ConstantHelper.FromFloat<T>(myFactory.NextSubContext(), 0f);
+            Default = theDefault ?? ConstantHelper.FromFloat<T>( 0f);
             
             SetProperty("ComputeSystemAttribute", this);
         }
@@ -46,10 +40,6 @@ namespace Fuse.compute
 
         public bool DoubleBuffered { get; }
         public string Group { get; }
-        public TextureAttributeType TextureAttributeType()
-        {
-            return compute.TextureAttributeType.Attribute;
-        }
 
         public void OverRideByTexture(ShaderNode<Texture> theInstance, ShaderNode<TIndex> theIndex)
         {
@@ -65,52 +55,8 @@ namespace Fuse.compute
             }
             CallChangeEvent();
         }
+
+        public override Int3 Resolution => TextureInput?.Value == null ? new Int3(1) : new Int3(TextureInput.Value.Width, TextureInput.Value.Height, TextureInput.Value.Depth);
     }
-    
-    public class TextureResource : Attribute<Texture>, ITextureAttribute
-    {
-        public TextureResource(NodeContext nodeContext, string theName) : base(nodeContext, theName, AttributeType.Texture)
-        {
-            AddProperty("ComputeSystemAttribute", this);
-        }
 
-        public string Group => Name;
-
-        public TextureAttributeType TextureAttributeType()
-        {
-            return compute.TextureAttributeType.Resource;
-        }
-
-        public bool DoubleBuffered => true;
-        
-        public TextureInput TextureInput{
-            get;
-            set;
-        }
-    }
-    
-    public class TextureInstance<T> : Attribute<T>, ITextureAttribute
-    {
-
-
-
-        public TextureInstance(NodeContext nodeContext, string theName) : base(nodeContext, theName, AttributeType.Texture)
-        {
-            AddProperty("ComputeSystemAttribute", this);
-        }
-
-        public string Group => Name;
-
-        public TextureAttributeType TextureAttributeType()
-        {
-            return compute.TextureAttributeType.Instance;
-        }
-        
-        public bool DoubleBuffered => true;
-        
-        public TextureInput TextureInput{
-            get;
-            set;
-        }
-    }
 }
