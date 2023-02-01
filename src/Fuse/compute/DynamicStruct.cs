28ecbfe7bff56bc3429c9b1ca43087edfeacde71
Christian Riekoff <christian@riekoff.com>
2023-02-01 14:49:42 +0100

    made dynamic struct more usable

--- src/Fuse/compute/DynamicStruct.cs
+++ src/Fuse/compute/DynamicStruct.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using System.Text;
 using Stride.Core.Extensions;
 using VL.Core;
+using VL.Lib.Adaptive;
 
 namespace Fuse.compute
 {
     
-    public class DynamicStruct: ShaderNode<GpuStruct>
+    public class DynamicStruct<T>: ShaderNode<T>
     {
-        private string _name;
+        private string _structName;
         
-        public DynamicStruct(NodeContext nodeContext) : base(nodeContext, "GPUAttributeStruct")
+        public DynamicStruct(NodeContext nodeContext, T instance) : base(nodeContext, "GPUAttributeStruct")
         {
             
         }
 
         public void SetInput(IEnumerable<AbstractShaderNode> theInputs, string theName)
         {
-            _name = theName;
+            _structName = theName;
             
             const string shaderCode = 
                 @"    struct ${structName}{
@@ -48,16 +49,65 @@ ${structMembers}
             SetInputs(theInputs);
         }
 
+        private string _sourceTemplate = "";
+
+        public void SetInput(Dictionary<string,AbstractShaderNode> theInputs)
+        {
+            //_name = typeof(T).Name;
+            _structName = TypeHelpers.GetGpuType<T>();
+            Name = ShaderNodesUtil.FirstLetterToLower(_structName);
+            
+            const string shaderCode = 
+                @"    struct ${structName}{
+${structMembers}
+    };" ;
+            
+            var myStride = 0;
+            var call = new StringBuilder();
+            foreach (var kv in theInputs)
+            {
+                call.Append("        "+TypeHelpers.GetGpuType(kv.Value) + " " + kv.Key+";"+Environment.NewLine);
+                myStride += TypeHelpers.GetSizeInBytes(kv.Value);
+            }
+            var structString = ShaderNodesUtil.Evaluate(shaderCode,new Dictionary<string, string>()
+            {
+                {"structName", _structName},
+                {"structMembers", call.ToString()}
+            });
+            
+            var template = new StringBuilder();
+            template.Append("${resultType} ${resultName};"+Environment.NewLine);
+            foreach (var kv in theInputs)
+            {
+                template.Append("        ${resultName}." + kv.Key + " = "+ kv.Value.ID+";"+Environment.NewLine);
+                myStride += TypeHelpers.GetSizeInBytes(kv.Value);
+            }
+            
+            _sourceTemplate = ShaderNodesUtil.Evaluate(template.ToString(),
+                new Dictionary<string, string>
+                {
+                    {"resultType", _structName}
+                });
+            
+            SetProperty(Structs, structString);
+            Stride = myStride;
+
+            TypeOverride = _structName;
+            
+            SetInputs(theInputs.Values);
+        }
+
         protected override string SourceTemplate()
         {
-            return "";
+            return _sourceTemplate;
+           
         }
 
         public List<string> Description {
             get
             {
                 var result = new List<string>();
-                Ins.ForEach(input => result.Add(_name + "." + TypeHelpers.GetDescription(input)));
+                Ins.ForEach(input => result.Add(_structName + "." + TypeHelpers.GetDescription(input)));
                 return result;
             }
         }
