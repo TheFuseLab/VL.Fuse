7048d9b9901b69f4093279f29a285f99c196cacc
Christian Riekoff <christian@riekoff.com>
2023-05-16 07:50:37 +0200

    add stateful region

--- src/Fuse/function/SDFFunction.cs
+++ src/Fuse/function/SDFFunction.cs
@@ -1,13 +1,39 @@
-﻿using Stride.Core.Mathematics;
+﻿using System;
+using Stride.Core.Mathematics;
 
 namespace Fuse.function;
 
 public static class FuseDelegates
 {
-    public delegate void SDFDelegate(in ShaderNode<Vector3> _domain, out ShaderNode<float> _distance);
     
-    public static void InvokeSDF(SDFDelegate _delegate, ShaderNode<Vector3> _domain, out ShaderNode<float> _distance)
+    public delegate void SDFUpdate( object stateInput, out object stateOutput, ShaderNode<Vector3> _domain, out ShaderNode<float> _distance);
+    
+    public delegate void SDFCreate( out object stateOutput);
+
+    public class SDFInvoke : IDisposable
     {
-        _delegate.Invoke(_domain, out _distance);
+        private object State;
+
+        private SDFUpdate UpdateDelegate;
+        public SDFInvoke()
+        {
+            
+        }
+
+        public void Update(SDFCreate create, SDFUpdate update)
+        {
+            if(State == null)create(out State);
+            UpdateDelegate = update;
+        }
+
+        public void Invoke(ShaderNode<Vector3> domain, out ShaderNode<float> distance)
+        {
+            UpdateDelegate(State, out State, domain, out distance);
+        }
+
+        public void Dispose()
+        {
+            if(State is IDisposable disposable) disposable.Dispose();
+        }
     }
 }
