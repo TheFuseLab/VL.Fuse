fbd4325c784995f5136dab73804bc4470bcf0520
Christian Riekoff <christian@riekoff.com>
2023-06-07 23:51:34 +0200

    add Delegate0In0Out

---
+++ src/Fuse/function/Delegate0In0Out.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Fuse.compute;
+using Fuse.function;
+using VL.Core;
+
+namespace Fuse.Function;
+
+
+public delegate void Delegate0In0OutUpdate<TOut>( object stateInput, out object stateOutput, out ShaderNode<TOut> Output);
+    
+public delegate void Delegate0In0OutCreate( out object stateOutput);
+
+public class Delegate0In0Out<TOut> : DelegateStateful<Delegate<TOut>>
+{
+
+    public Delegate0In0Out(NodeContext theNodeContext) : base(theNodeContext)
+    { 
+    }
+
+    public void Update(Delegate0In0OutCreate create, Delegate0In0OutUpdate<TOut> update)
+    {
+        if(State == null)create(out State);
+        
+        var nodeSubContextFactory = new NodeSubContextFactory(NodeContext);
+
+        update(State, out State, out var outArgument);
+
+        Delegate = new Delegate<TOut>(
+            nodeSubContextFactory.NextSubContext(),
+            outArgument,
+            new List<IFunctionParameter>());
+    }
+}
+
+public class Delegate0In0OutInvoke<TOut> : Invoke<TOut>
+{
+    public Delegate0In0OutInvoke(
+        NodeContext nodeContext, 
+        Delegate0In0Out<TOut> theDelegate) : base(nodeContext,theDelegate?.Delegate, new List<AbstractShaderNode>())
+    {
+        
+    }
+}
