183c88658faf1ffd65010812d766be539ec2758d
Christian Riekoff <christian@riekoff.com>
2023-08-15 07:41:36 +0200

    add interlocked for texture

--- src/Fuse/compute/Interlocked.cs
+++ src/Fuse/compute/Interlocked.cs
@@ -4,7 +4,7 @@ using VL.Stride.Shaders.ShaderFX;
 
 namespace Fuse.compute;
 
-public class Interlocked : ShaderNode<GpuVoid>, IComputeVoid
+public class InterlockedBuffer : ShaderNode<GpuVoid>, IComputeVoid
 {
     private readonly IBufferInput<int> _buffer;
     private readonly ShaderNode<int> _index;
@@ -12,7 +12,7 @@ public class Interlocked : ShaderNode<GpuVoid>, IComputeVoid
     private readonly ShaderNode<int> _lastValue;
     private readonly string _function;
     
-    public Interlocked(NodeContext nodeContext, string theFunction, IBufferInput<int> theBuffer, ShaderNode<int> theIndex, ShaderNode<int> theValue) : base( nodeContext, "interlocked")
+    public InterlockedBuffer(NodeContext nodeContext, string theFunction, IBufferInput<int> theBuffer, ShaderNode<int> theIndex, ShaderNode<int> theValue) : base( nodeContext, "interlocked")
     {
         var factory = new NodeSubContextFactory(NodeContext);
         _lastValue = new DeclareValue<int>(factory.NextSubContext());
@@ -43,6 +43,52 @@ public class Interlocked : ShaderNode<GpuVoid>, IComputeVoid
         {
             {"function", _function},
             {"bufferName", _buffer.ID},
+            {"index", _index.ID},
+            {"value", _value.ID},
+            {"lastValue", _lastValue.ID}
+        });
+    }
+}
+
+public class InterlockedTexture : ShaderNode<GpuVoid>, IComputeVoid
+{
+    private readonly ITextureInput _texture;
+    private readonly ShaderNode<int> _index;
+    private readonly ShaderNode<int> _value;
+    private readonly ShaderNode<int> _lastValue;
+    private readonly string _function;
+    
+    public InterlockedTexture(NodeContext nodeContext, string theFunction, ITextureInput theTexture, ShaderNode<int> theIndex, ShaderNode<int> theValue) : base( nodeContext, "interlocked")
+    {
+        var factory = new NodeSubContextFactory(NodeContext);
+        _lastValue = new DeclareValue<int>(factory.NextSubContext());
+        OptionalOutputs = new List<AbstractShaderNode> { _lastValue} ;
+        
+        _function = theFunction;
+        _texture = theTexture;
+        _index = theIndex;
+        _value = theValue;
+            
+        SetInputs(new List<AbstractShaderNode> {_texture as AbstractShaderNode,_index, _value,_lastValue});
+    }
+        
+    protected override Dictionary<string, string> CreateTemplateMap()
+    {
+        return new Dictionary<string, string>();
+    }
+        
+    protected override string GenerateDefaultSource()
+    {
+        return "";
+    }
+
+    protected override string SourceTemplate()
+    {
+        const string shaderCode = "${function}(${textureName}[${index}], ${value}, ${lastValue});";
+        return ShaderNodesUtil.Evaluate(shaderCode,new Dictionary<string, string>
+        {
+            {"function", _function},
+            {"textureName", _texture.TextureID},
             {"index", _index.ID},
             {"value", _value.ID},
             {"lastValue", _lastValue.ID}
