b1a8c24273cd949ead02ee93cfa8f2c1843b9dae
Christian Riekoff <christian@riekoff.com>
2024-03-01 12:18:03 +0100

    add spectrum enum

--- src/Fuse/enums/Fuse.Color.cs
+++ src/Fuse/enums/Fuse.Color.cs
@@ -3,5 +3,15 @@
 namespace Fuse.Color
 {
     public enum ColorSpace { RGB, HSL, HSV, LAB, SRGB };
+    
+    public enum Spectrum
+    {
+        SpectralBruton,
+        SpectralGems,
+        SpectralJet,
+        SpectralZucconi6,
+        SpectralZucconi,
+        SpectralSpektre
+    }
 
 }
