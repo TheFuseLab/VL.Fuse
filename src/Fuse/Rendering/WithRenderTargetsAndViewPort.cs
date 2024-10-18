1b79eafac33a25dfb2144816f27952da3e8479bd
Christian Riekoff <christian@riekoff.com>
2024-10-18 14:23:20 +0200

    Add rendertexture with multiple rendertargets

--- src/Fuse/Rendering/WithRenderTargetsAndViewPort.cs
+++ src/Fuse/Rendering/WithRenderTargetsAndViewPort.cs
@@ -1,6 +1,52 @@
-﻿namespace Fuse.Rendering;
+﻿using System.Collections.Generic;
+using Stride.Graphics;
+using Stride.Rendering;
+using RendererBase = VL.Stride.Rendering.RendererBase;
 
-public class WithRenderTargetsAndViewPort
-{
-    
-}
+namespace Fuse.Rendering;
+
+public class WithRenderTargetAndViewPort : RendererBase
+    {
+        ViewportState viewportState = new ViewportState();
+
+        public Texture RenderTarget { get; set; }
+
+        public Texture DepthBuffer { get; set; }
+
+        protected override void DrawInternal(RenderDrawContext context)
+        {
+            var renderTarget = RenderTarget;
+            var depthBuffer = DepthBuffer;
+            var setRenderTarget = renderTarget != null;
+            var setDepthBuffer = depthBuffer != null;
+
+            if (setRenderTarget || setDepthBuffer)
+            {
+                var renderContext = context.RenderContext;
+
+                using (renderContext.SaveRenderOutputAndRestore())
+                using (renderContext.SaveViewportAndRestore())
+                using (context.PushRenderTargetsAndRestore())
+                {
+                    if (setRenderTarget)
+                    {
+                        renderContext.RenderOutput.RenderTargetFormat0 = renderTarget.ViewFormat;
+                        renderContext.RenderOutput.RenderTargetCount = 1;
+
+                        renderContext.ViewportState = viewportState;
+                        renderContext.ViewportState.Viewport0 = new Viewport(0, 0, renderTarget.ViewWidth, renderTarget.ViewHeight);
+                    }
+                    //context.CommandList.SetViewports();
+                    //context.CommandList.SetRenderTargets();
+                    context.CommandList.SetRenderTargetAndViewport(depthBuffer, renderTarget);
+                    
+                    DrawInput(context);
+                    
+                }  
+            }
+            else
+            {
+                DrawInput(context);
+            }
+        }
+    }
