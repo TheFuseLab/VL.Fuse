5360f7b45564ba3742e26cbc2466b35782905239
Christian Riekoff <christian@riekoff.com>
2025-02-07 11:05:41 +0100

    Restructure Calculus enums

--- src/Fuse/enums/Fuse.Calculus.cs
+++ src/Fuse/enums/Fuse.Calculus.cs
@@ -2,11 +2,38 @@
 
 namespace Fuse.Calculus
 {
-    public enum GradientDifferentiationMode { ForwardDifference, CentralDifference, FivePointStencil };
+    public enum GradientDifferentiationMode
+    {
+        ForwardDifference, 
+        CentralDifference, 
+        FivePointStencil
+    };
 
-    public enum IntegrationMode { Euler, RungeKutta2, RungeKutta4 };
+    public enum IntegrationMode
+    {
+        Euler, 
+        RungeKutta2, 
+        RungeKutta4
+    };
     
-    
-    public enum ComputeIntegrationMode { Direct, Euler, Verlet, LeapFrog, RungeKutta2, RungeKutta4 };
+    public enum ComputeIntegrationMode
+    {
+        EulerForward, 
+        EulerAccel,
+        VerletAccel, 
+    };
+
+
+    public enum ComputeIntegrationModeRegion
+    {
+        EulerForward, 
+        RungeKutta2Forward, 
+        RungeKutta4Forward,
+        EulerAccel,
+        VerletAccel, 
+        LeapFrogAccel, 
+        RungeKutta2Accel, 
+        RungeKutta4Accel
+    };
 
 }
